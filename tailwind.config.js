880f44b91a7b568505fe746e84fe3bbfa5ec5414
Alex <alex@gu3.me>
2021-05-05 12:12:35 +0100

    Experimental: move over to Tailwind CSS Remove Spotify & Toast components Delete unused image Remove nuxt/content

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    minWidth: {
+      '1/2': '50%',
+      '350': '350px'
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
