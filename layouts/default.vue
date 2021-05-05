00691196c234eb2a076804cf0155b6b6bbecc70f
Alex <alex@gu3.me>
2021-05-05 13:46:12 +0100

    Finish replacing all scss with tailwind

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,80 +1,63 @@
 <template>
-  <div>
+  <div class="">
     <Nuxt />
   </div>
 </template>
 
-<script>
-  export default {
-  }
-</script>
-
-<style lang="scss">
+<style>
 @import url("~/assets/normalize.css");
+
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+
+* {
+  @apply box-border;
+}
+
+@layer base {
   body {
-    font-family: -apple-system, BlinkMacSystemFont, "Inter", sans-serif;
+    @apply text-base;
 
-    line-height: 1.5;
-    padding: 0;
-    margin: 0;
+    @apply m-0;
+    @apply animate-fade;
 
-    padding-top: env(safe-area-inset-top);
-    padding-bottom: env(safe-area-inset-bottom);
-    padding-left: env(safe-area-inset-left);
-    padding-right: env(safe-area-inset-right);
+    @apply p-safe;
+    @apply font-sans;
 
-    animation: fadeIn 500ms ease;
-  }
+    @apply text-black;
+    @apply bg-white;
 
-  html {
-    transition: background 150ms ease, color 150ms ease;
+    @apply leading-normal;
+    @apply font-normal;
 
-    font-size: 16px;
-    word-spacing: 1px;
-    font-weight: 400;
-      
-    background: #FFFFFF;
-    color: #202020;
-
-    @media screen and (prefers-color-scheme: dark) {
-      /* Dark mode */
-      background: #202020;
-      color: #ffffff;
-      font-weight: 350; // Thinner font; easier to read on dark background
+    &.dark {
+      @apply text-white;
+      @apply bg-gray-800;
+      @apply font-350;
     }
-
-    line-height: 1.5;
   }
 
-  * {
-    box-sizing: border-box;
+  a {
+    @apply transition-colors;
+    @apply no-underline;
+    @apply text-inherit;
+    &:hover {
+      @apply text-gray-700;
+    }
   }
 
   h1, h2, h3, h4, h5, h6 {
-    margin: 1.5em 0;
-
-    @media screen and (prefers-color-scheme: dark) {
-      font-weight: 650;
+    @apply font-bold;
+    @apply my-12;
+    
+    &.dark {
+      @apply font-650;
     }
   }
 
   p {
-    margin-bottom: 0.75em;
+    @apply mb-4;
   }
-
-  a {
-    color: inherit;
-    text-decoration: none;
-
-    transition: color ease 150ms;
-
-    &:hover {
-        color: #888888;
-    }
-  }
-
-  @keyframes fadeIn {
-    0% { opacity: 0; }
-    100% { opacity: 1; }
-  }
+}
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,49 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    minWidth: {
-      '1/2': '50%',
-      '350': '350px'
-    }
+    extend: {
+      minWidth: {
+        '1/2': '50%',
+        '350': '350px'
+      },
+      fontFamily: {
+        'sans': [ '-apple-system', 'BlinkMacSystemFont', '"Inter"', 'sans-serif']
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 }
+        }
+      },
+      animation: {
+        'fade': 'fadeIn 500ms ease'
+      },
+      textColor: {
+        'keybase': '#FF6F21',
+        'email': '#d43434',
+        'spotify': '#1DB954',
+        'blog': '#2181ff',
+        'youtube': '#FF0000',
+        'discord': '#7289DA',
+        'uwutube': '#fe71dc',
+        'black': '#202020',
+        'inherit': 'inherit'
+      },
+      backgroundColor: {
+        'black': '#202020'
+      },
+      fontWeight: {
+        350: 350,
+        650: 650
+      }
+    },
+    padding: {
+      'safe': 'env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left)',
+      'safe-top': 'env(safe-area-inset-top)',
+      'safe-bottom': 'env(safe-area-inset-bottom)',
+      'safe-right': 'env(safe-area-inset-right)',
+      'safe-left': 'env(safe-area-inset-left)'
+    },
   },
   variants: {
     extend: {},
