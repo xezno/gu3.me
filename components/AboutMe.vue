3efc92accdbe2681ab36235caba7b710b8a3ef2c
Alex <alex@gu3.me>
2021-05-06 15:08:22 +0100

    Improved "about me" section

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -3,20 +3,59 @@
     <div class="flex">
       <BaseCard title="Things I Like" class="mr-5 w-1/2">
         <ul>
-          <li><b>Reverse Engineering</b> - Experience in </li>
-          <li><b>Computer Graphics</b> - Skills creating interactive webapps and providing functionality to otherwise static pages</li>
-          <li><b>Game Development</b> - Skills creating uniquely stylized webapps for mobile and desktop platforms</li>
+          <li>
+            <b>Reverse Engineering</b>
+            <p>Experienced in de-constructing and re-implementing various file formats for 
+            abandonware titles</p>
+          </li>
+          <li>
+            <b>Computer Graphics</b>
+            <p>Skills writing OpenGL and Vulkan graphics code for desktop applications and games</p>
+          </li>
+          <li>
+            <b>Game Development</b>
+            <p>Skills creating fun video game experiences tailored to video game enthusiasts</p>
+          </li>
+          <li>
+            <b>Web Development</b>
+            <p>Experienced in creating small- and large-scale websites, for both organisations and individuals</p>
+          </li>
         </ul>
       </BaseCard>
-      <BaseCard title="My Programming Experience" class="ml-5 w-1/2">
+      <BaseCard title="My Programming Experience" class="ml-5 w-1/2 leading-relaxed">
         <ul>
-          <li><b>C#</b> - Experience writing C# code for use within Unity and as desktop applications</li>
-          <li><b>JavaScript</b> - Skills creating interactive webapps and providing functionality to otherwise static pages</li>
-          <li><b>CSS/SCSS</b> - Skills creating uniquely stylized webapps for mobile and desktop platforms</li>
-          <li><b>C++</b> - Experience writing high-performance native desktop code</li>
-          <li><b>Wren</b> - Skills in using as an embedded scripting language within other applications</li>
+          <li>
+            <b>C#</b> 
+            <p>Experienced in writing C# code for use within Unity and as desktop applications</p>
+          </li>
+          <li>
+            <b>JavaScript</b> 
+            <p>Skills creating interactive webapps and providing functionality to otherwise static pages</p>
+          </li>
+          <li>
+            <b>CSS/SCSS</b> 
+            <p>Skills creating uniquely stylized webapps for mobile and desktop platforms</p>
+          </li>
+          <li>
+            <b>C++</b> 
+            <p>Experienced in writing high-performance native desktop code</p>
+          </li>
         </ul>
       </BaseCard>
     </div>
   </BaseArea>
 </template>
+
+<style lang="postcss" scoped>
+@tailwind base;
+@tailwind utilities;
+@tailwind components;
+
+@layer base {
+  ul {
+    & li {
+      @apply mt-4;
+    }
+  }
+}
+</style>
