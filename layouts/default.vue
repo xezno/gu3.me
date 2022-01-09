4ef306f42af4151261a088425756cf17f7cb9094
Alex <alex@gu3.me>
2022-01-09 15:59:16 +0000

    New year

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,7 @@
 
     <footer class="text-center md:text-left font-bold flex md:content-evenly p-5 uppercase bg-dark-800">
       <div class="w-full md:w-96 m-auto">
-        &copy; Alex Guthrie 2021
+        &copy; Alex Guthrie 2022
       </div>
       <div class="w-0 invisible md:visible md:w-96 m-auto text-right">
         <SocialLinks class="justify-end"></SocialLinks>
