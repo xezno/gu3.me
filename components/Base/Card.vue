4087d9880c9d3c574e6f3b31eb98fd1200d44123
Alex <alex@gu3.me>
2022-03-04 01:05:34 +0000

    Card.vue cleanup

--- components/Base/Card.vue
+++ components/Base/Card.vue
@@ -9,35 +9,11 @@
 </template>
 
 <script>
-import { faTag, faArrowRight, faCalendar } from "@fortawesome/free-solid-svg-icons";
 export default {
   props: {
     title: {
       type: String,
       default: "Untitled"
-    },
-    image: {
-      type: String,
-      default: null
-    },
-    link: {
-      type: String,
-      default: null
-    },
-    date: {
-      type: String,
-      default: null
-    },
-    tags: {
-      type: Array,
-      default: null
-    }
-  },
-  data: function() {
-    return {
-      faTag,
-      faArrowRight,
-      faCalendar
     }
   }
 }
@@ -46,7 +22,6 @@ export default {
 <style lang="scss" scoped>
 @import "~assets/variables.scss";
 
-// bg-dark-800 border-dark-600 border-opacity-20 shadow-lg border border-solid rounded-lg p-5 my-10 overflow-hidden group
 .card {
   background: $dark-800;
   border: 1px solid rgba( $dark-600, 0.2 );
