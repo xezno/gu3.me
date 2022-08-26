fde0f8a5fcccd3e1db185c93af5f8a9319f46df3
Alex <alex@gu3.me>
2022-08-26 03:48:44 +0100

    Use single column layout for skills section

--- components/Section/AboutMe.vue
+++ components/Section/AboutMe.vue
@@ -5,41 +5,25 @@
         <tr>
           <td>
             <h4>C#</h4>
-          </td>
-          <td>
             I've written tons of C# code for use within games (s&amp;box, Unity) and desktop applications
           </td>
         </tr>
         <tr>
           <td>
             <h4>Rust, C++</h4>
-          </td>
-          <td>
             I've written high-performance native desktop code using Rust as well as C++
           </td>
         </tr>
         <tr>
           <td>
             <h4>OpenGL, DirectX</h4>
-          </td>
-          <td>
-            I've written OpenGL and DirectX 11/12 graphics code for desktop applications and games
+            I've written OpenGL and DirectX graphics code for desktop applications and games
           </td>
         </tr>
         <tr>
           <td>
             <h4>HTML, CSS/SCSS, JavaScript</h4>
-          </td>
-          <td>
             I've made cool webapp experiences, ranging from websites to games using WebGL
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <h4>Node - JavaScript, TypeScript</h4>
-          </td>
-          <td>
-            I have experience writing node-based backend applications using both JavaScript and TypeScript
           </td>
         </tr>
       </table>
