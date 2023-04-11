0a746ab68a606c3098c8dc1602b1fa472fbc0055
Alex <alex@gu3.me>
2023-04-11 03:48:40 +0100

    Preload the correct font...

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,8 +30,8 @@ export default defineNuxtConfig({
             link: [
                 { rel: 'shortcut icon', type: 'image/png', href: '/img/robot.png' },
                 { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-                { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap' },
-                { rel: 'preload', as: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap' }
+                { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@100;200;300;400;500;600;700;800;900&display=swap' },
+                { rel: 'preload', as: 'style', href: 'https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@100;200;300;400;500;600;700;800;900&display=swap' }
             ]
         },
     }
